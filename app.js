7f39b8c9a9b4157ed07a79a6fe25d53cc922af55
Crynowecer0 <crynowecer11@gmail.com>
2023-05-08 19:37:15 -0400

    complete part one

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 "use strict";
 
+//jQuery objects
+const $body = $("body");
 
+/**
+ * make 4 calls to the numbers API and return an array of response objects
+ */
 async function multipleCalls() {
-    const number1 = axios({
-        method: 'get',
-        url: 'http://numbersapi.com/11?json'
-      });
-    const number2 = axios({
-        method: 'get',
-        url: 'http://numbersapi.com/12?json'
-      });
-    const numberRandom = axios({
-        method: 'get',
-        url: 'http://numbersapi.com/random/trivia?json'
-      });
+  const number1 = axios({
+    method: "get",
+    url: "http://numbersapi.com/14?json",
+  });
 
-    let response = await Promise.all([number1, number2, numberRandom]);
+  const number2 = axios({
+    method: "get",
+    url: "http://numbersapi.com/11?json",
+  });
 
-    console.log("response=", response);
-    return response;
+  const numberRandom = axios({
+    method: "get",
+    url: "http://numbersapi.com/random/trivia?json",
+  });
+
+  const numberRandom2 = axios({
+    method: "get",
+    url: "http://numbersapi.com/random/trivia?json",
+  });
+//throws errors if ANY fails -> use try/catch here
+  let response = await Promise.all([
+    number1,
+    number2,
+    numberRandom,
+    numberRandom2,
+  ]);
+
+  console.log("response=", response);
+  return response;
 }
+
+/**
+ * append the data contained with an array of response objects to the DOM
+ */
+async function renderFacts() {
+  const facts = await multipleCalls();
+
+  const $facts = $("<ul>").append(
+    facts.map((result) => $("<li>").text(result.data.text))
+  );
+
+  $body.append($facts)
+}
+
+renderFacts()
+
